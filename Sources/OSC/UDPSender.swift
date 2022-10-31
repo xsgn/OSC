78473dc9eef29f91749fef890733689db29bab17
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2022-10-31 14:50:08 +0900

    update

--- Sources/OSC/UDPSender.swift
+++ Sources/OSC/UDPSender.swift
@@ -6,14 +6,73 @@
 //
 import Network
 public struct UDPSender {
-    static func send(host: String, port: UInt16, message: Message) {
+    private let connection: NWConnection
+    init(to target: (String, UInt16)) async throws {
+        let (host, port) = target
+        connection = .init(host: .init(host), port: .init(integerLiteral: port), using: .udp)
+        connection.start(queue: .global(qos: .utility))
+    }
+    func send(message: Message) async throws {
+        try await withCheckedThrowingContinuation { (future: CheckedContinuation<Void, Error>) in
+            connection.send(content: .init(message: message), completion: .contentProcessed {
+                switch $0 {
+                case.none:
+                    future.resume(returning: ())
+                case.some(let error):
+                    future.resume(throwing: error)
+                }
+            })
+        }
+    }
+    static func send(host: String, port: UInt16, message: Message) async throws {
+        try await withCheckedThrowingContinuation { (future: CheckedContinuation<Void, Error>) in
+            let connection = NWConnection(host: .init(host), port: .init(integerLiteral: port), using: .udp)
+            connection.stateUpdateHandler  = { state in
+                print(state)
+                switch state {
+                case.ready:
+                    connection.send(content: .init(message: message), completion: .contentProcessed {
+                        switch $0 {
+                        case.some(let error):
+                            future.resume(throwing: error)
+                        case.none:
+                            future.resume(returning: ())
+                        }
+                    })
+                case.failed(let error), .waiting(let error):
+                    future.resume(throwing: error)
+                case.cancelled:
+                    future.resume(throwing: NWError.posix(.ECANCELED))
+                case.preparing, .setup:
+                    break
+                @unknown default:
+                    fatalError()
+                }
+            }
+            connection.start(queue: .global(qos: .utility))
+        }
+    }
+    static func send(host: String, port: UInt16, message: Message, future: @escaping(Void?, NWError?) -> Void) {
         let connection = NWConnection(host: .init(host), port: .init(integerLiteral: port), using: .udp)
-        connection.stateUpdateHandler = { state in
+        connection.stateUpdateHandler  = { state in
             switch state {
             case.ready:
-                try!connection.send(content: .init(message: message), completion: .idempotent)
-            default:
+                connection.send(content: .init(message: message), completion: .contentProcessed {
+                    switch $0 {
+                    case.some(let error):
+                        future(.none, .some(error))
+                    case.none:
+                        future(.some(()), .none)
+                    }
+                })
+            case.failed(let error), .waiting(let error):
+                future(.none, .some(error))
+            case.cancelled:
+                future(.none, .some(.posix(.ECANCELED)))
+            case.preparing, .setup:
                 break
+            @unknown default:
+                fatalError()
             }
         }
         connection.start(queue: .global(qos: .utility))
--- Tests/OSCTests/UDPReceiverTests.swift
+++ Tests/OSCTests/UDPReceiverTests.swift
@@ -6,14 +6,15 @@
 //
 import XCTest
 @testable import OSC
+import AVFoundation
 final class UDPReceiverTestCases: XCTestCase {
-    final func testSend() {
+    final func testSend() async throws {
         var message = Message(address: "/filter")
         message.append(10.0)
         message.append("hello")
         message.append(200)
-        UDPSender.send(host: "127.0.0.1", port: 5005, message: message)
-        RunLoop.current.run()
+        try await UDPSender(to: ("127.0.0.1", 5005))
+            .send(message: message)
     }
 //    final func testBind() throws {
 //        try withExtendedLifetime(UDPr(host: "127.0.0.1", port: 5005), RunLoop.main.run)
