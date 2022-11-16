a125162dc8be118129cc170209f5d9ca25849123
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2022-11-16 15:32:31 +0900

    compact implementation

--- Sources/OSC/UDPSender.swift
+++ Sources/OSC/UDPSender.swift
@@ -4,77 +4,23 @@
 //
 //  Created by kotan.kn on 10/27/22.
 //
-import Network
-public struct UDPSender {
-    private let connection: NWConnection
-    init(to target: (String, UInt16)) async throws {
-        let (host, port) = target
-        connection = .init(host: .init(host), port: .init(integerLiteral: port), using: .udp)
-        connection.start(queue: .global(qos: .utility))
+import Foundation.NSData
+public final class UDPSender {
+    private let fd: Int32
+    public init() {
+        fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
     }
-    func send(message: Message) async throws {
-        try await withCheckedThrowingContinuation { (future: CheckedContinuation<Void, Error>) in
-            connection.send(content: .init(message: message), completion: .contentProcessed {
-                switch $0 {
-                case.none:
-                    future.resume(returning: ())
-                case.some(let error):
-                    future.resume(throwing: error)
-                }
-            })
-        }
+    deinit {
+        close(fd)
     }
-    static func send(host: String, port: UInt16, message: Message) async throws {
-        try await withCheckedThrowingContinuation { (future: CheckedContinuation<Void, Error>) in
-            let connection = NWConnection(host: .init(host), port: .init(integerLiteral: port), using: .udp)
-            connection.stateUpdateHandler  = { state in
-                print(state)
-                switch state {
-                case.ready:
-                    connection.send(content: .init(message: message), completion: .contentProcessed {
-                        switch $0 {
-                        case.some(let error):
-                            future.resume(throwing: error)
-                        case.none:
-                            future.resume(returning: ())
-                        }
-                    })
-                case.failed(let error), .waiting(let error):
-                    future.resume(throwing: error)
-                case.cancelled:
-                    future.resume(throwing: NWError.posix(.ECANCELED))
-                case.preparing, .setup:
-                    break
-                @unknown default:
-                    fatalError()
-                }
-            }
-            connection.start(queue: .global(qos: .utility))
-        }
-    }
-    static func send(host: String, port: UInt16, message: Message, future: @escaping(Void?, NWError?) -> Void) {
-        let connection = NWConnection(host: .init(host), port: .init(integerLiteral: port), using: .udp)
-        connection.stateUpdateHandler  = { state in
-            switch state {
-            case.ready:
-                connection.send(content: .init(message: message), completion: .contentProcessed {
-                    switch $0 {
-                    case.some(let error):
-                        future(.none, .some(error))
-                    case.none:
-                        future(.some(()), .none)
-                    }
-                })
-            case.failed(let error), .waiting(let error):
-                future(.none, .some(error))
-            case.cancelled:
-                future(.none, .some(.posix(.ECANCELED)))
-            case.preparing, .setup:
-                break
-            @unknown default:
-                fatalError()
+}
+extension UDPSender {
+    public final func send(to: (String, UInt16), data: Data) {
+        let sent = sockaddr_in(host: to.0, port: to.1).Sockaddr { (mem, len) in
+            data.withUnsafeBytes {
+                sendto(fd, $0.baseAddress, $0.count, 0, mem, len.pointee)
             }
         }
-        connection.start(queue: .global(qos: .utility))
+        assert(sent == data.count)
     }
 }
